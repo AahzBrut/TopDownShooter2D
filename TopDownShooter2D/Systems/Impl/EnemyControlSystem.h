50f6993b74ffad478fec991e00b16d6cb5dca25a
artemenko_pa <artemenko_pa@magnit.ru>
2025-01-30 14:29:21 +0300

    Code cleanup

--- TopDownShooter2D/Systems/Impl/EnemyControlSystem.h
+++ TopDownShooter2D/Systems/Impl/EnemyControlSystem.h
@@ -1,17 +1,27 @@
 #pragma once
 #include <flecs.h>
+#include <raymath.h>
 
 
 inline void EnemyControlSystem(const flecs::world &ecsWorld) {
     const auto playerQuery = ecsWorld.query_builder<const Position>().with<Player>().build();
 
     ecsWorld.system<const Position, Rotation, Velocity>()
-        .with<Enemy>()
-        .each([playerQuery](const Position &position, Rotation &rotation, Velocity &velocity) {
-            if (const auto playerEntity = playerQuery.find([](const Position &){ return true; })) {
-                const auto playerPosition = playerEntity.get<const Position>();
-                velocity.velocity = Vector2Normalize(playerPosition->position - position.position) * 50.0f;
-                rotation.rotation = Vector2Angle(Vector2(0, -1), velocity.velocity) * RAD2DEG;
-            }
-    });
+            .with<Enemy>()
+            .run([playerQuery](flecs::iter &it) {
+                playerQuery.each([&it](const Position &playerPosition) {
+                    while (it.next()) {
+                        const auto enemyPosition = it.field<const Position>(0);
+                        auto enemyRotation = it.field<Rotation>(1);
+                        auto enemyVelocity = it.field<Velocity>(2);
+                        for (const auto enemyId: it) {
+                            enemyVelocity[enemyId].velocity = Vector2Normalize(
+                                                                  playerPosition.position - enemyPosition[enemyId].
+                                                                  position) * 50.0f;
+                            enemyRotation[enemyId].rotation = Vector2Angle(Vector2(0, -1),
+                                                                           enemyVelocity[enemyId].velocity) * RAD2DEG;
+                        }
+                    }
+                });
+            });
 }
