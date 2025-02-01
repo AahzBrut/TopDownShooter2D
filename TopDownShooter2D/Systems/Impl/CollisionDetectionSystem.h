62ed95626db35231f44bdf6b0fadb73b9bc7f38b
artemenko_pa <artemenko_pa@magnit.ru>
2025-02-01 09:27:46 +0300

    Refactor collision detection system

--- TopDownShooter2D/Systems/Impl/CollisionDetectionSystem.h
+++ TopDownShooter2D/Systems/Impl/CollisionDetectionSystem.h
@@ -1,40 +1,10 @@
 #pragma once
 #include <flecs.h>
-#include <raymath.h>
 
-#include "Logger.h"
 #include "Components/Impl/Collider.h"
 #include "Components/Impl/Position.h"
 
 
-inline bool IsIntersects(const Position position1, const Collider collider1, const Position position2, const Collider collider2) {
-    if (!CollisionLayersSettings::IsLayersCollides(collider1.collisionLayer, collider2.collisionLayer)) return false;
-    const auto [distanceX, distanceY] = position1.position - position2.position;
-    const auto squaredDistance = distanceX * distanceX + distanceY * distanceY;
-    return squaredDistance <= collider1.radius * collider1.radius + collider2.radius * collider2.radius;
-}
+bool IsIntersects(Position position1, Collider collider1, Position position2, Collider collider2);
 
-inline void CollisionDetectionSystem(const flecs::world &ecsWorld) {
-    const auto query = ecsWorld.query<const Position, const Collider>();
-    const auto playerQuery = ecsWorld.query<const Player>();
-
-    ecsWorld.system<const Position, const Collider>(__func__)
-            .each([&ecsWorld, query, playerQuery](const flecs::entity entity1, const Position &position1, const Collider &collider1) {
-                query.each([&ecsWorld, playerQuery, entity1, position1, collider1](
-                const flecs::entity entity2, const Position &position2, const Collider &collider2) {
-                        if (entity1 >= entity2) return;
-                        if (IsIntersects(position1, collider1, position2, collider2)) {
-                            if (collider1.IsEnemy() && collider2.IsBullet() || collider1.IsBullet() && collider2.IsEnemy() ) {
-                                LOG("Bullet hits enemy");
-                                if (const auto playerEntity = playerQuery.find([](const Player &) { return true; })) {
-                                    const auto playerScore = playerEntity.get_mut<Score>();
-                                    playerScore->amount += 10;
-                                }
-                            }
-
-                            entity1.destruct();
-                            entity2.destruct();
-                        }
-                    });
-            });
-}
+void CollisionDetectionSystem(const flecs::world &ecsWorld);
