6f02a84fdaa5dadf1a0183c42d79c50eec2eb0c0
artemenko_pa <artemenko_pa@magnit.ru>
2025-02-02 08:18:30 +0300

    Optimize collision detection system

--- TopDownShooter2D/Systems/Impl/CollisionDetectionSystem.cpp
+++ TopDownShooter2D/Systems/Impl/CollisionDetectionSystem.cpp
@@ -9,10 +9,12 @@
 #include "Components/Impl/Score.h"
 #include "Components/Impl/Shotgun.h"
 #include "Components/Impl/SubmachineGun.h"
+#include "Components/Impl/Velocity.h"
 #include "Spawners/BonusSpawner.h"
 
 
-bool IsIntersects(const Position position1, const Collider collider1, const Position position2, const Collider collider2) {
+bool IsIntersects(const Position position1, const Collider collider1, const Position position2,
+                  const Collider collider2) {
     if (!CollisionLayersSettings::IsLayersCollides(collider1.collisionLayer, collider2.collisionLayer)) return false;
     const auto [distanceX, distanceY] = position1.position - position2.position;
     const auto squaredDistance = distanceX * distanceX + distanceY * distanceY;
@@ -24,20 +26,25 @@ void CollisionDetectionSystem(const flecs::world &ecsWorld) {
     const auto playerQuery = ecsWorld.query<const Player>();
 
     ecsWorld.system<const Position, const Collider>(__func__)
-            .each([&ecsWorld, query, playerQuery](const flecs::entity entity1, const Position &position1, const Collider &collider1) {
+            .with<Velocity>()
+            .each([&ecsWorld, query, playerQuery](const flecs::entity entity1, const Position &position1,
+                                                  const Collider &collider1) {
                 query.each([&ecsWorld, playerQuery, entity1, position1, collider1](
                 const flecs::entity entity2, const Position &position2, const Collider &collider2) {
                         if (entity1 >= entity2) return;
                         if (IsIntersects(position1, collider1, position2, collider2)) {
-                            if (collider1.IsEnemy() && collider2.IsBullet() || collider1.IsBullet() && collider2.IsEnemy() ) {
+                            if (collider1.IsEnemy() && collider2.IsBullet() || collider1.IsBullet() && collider2.
+                                IsEnemy()) {
                                 LOG("Bullet hits enemy");
-                                const auto enemyPosition = collider1.IsEnemy() ? position1.position : position2.position;
+                                const auto enemyPosition =
+                                        collider1.IsEnemy() ? position1.position : position2.position;
                                 if (const auto playerEntity = playerQuery.find([](const Player &) { return true; })) {
                                     const auto playerScore = playerEntity.get_mut<Score>();
                                     playerScore->amount += 10;
                                     SpawnBonus(ecsWorld, enemyPosition);
                                 }
-                            } else if (collider1.IsPlayer() && collider2.IsBonus() || collider1.IsBonus() && collider2.IsPlayer() ) {
+                            } else if (collider1.IsPlayer() && collider2.IsBonus() || collider1.IsBonus() && collider2.
+                                       IsPlayer()) {
                                 const auto playerEntity = collider1.IsPlayer() ? entity1 : entity2;
                                 const auto bonusEntity = collider1.IsBonus() ? entity1 : entity2;
                                 switch (const auto bonus = bonusEntity.get<Bonus>(); bonus->bonusType) {
@@ -64,4 +71,3 @@ void CollisionDetectionSystem(const flecs::world &ecsWorld) {
                     });
             });
 }
-
