cc46870ad671d02426e767719692e0125be93cca
artemenko_pa <artemenko_pa@magnit.ru>
2025-02-02 08:23:35 +0300

    Optimize collision detection system

--- TopDownShooter2D/Systems/Impl/CollisionDetectionSystem.cpp
+++ TopDownShooter2D/Systems/Impl/CollisionDetectionSystem.cpp
@@ -21,6 +21,26 @@ bool IsIntersects(const Position position1, const Collider collider1, const Posi
     return squaredDistance <= collider1.radius * collider1.radius + collider2.radius * collider2.radius;
 }
 
+void ApplyBonus(const flecs::entity entity1, const Collider collider1, const flecs::entity entity2) {
+    const auto playerEntity = collider1.IsPlayer() ? entity1 : entity2;
+    const auto bonusEntity = collider1.IsBonus() ? entity1 : entity2;
+    switch (const auto bonus = bonusEntity.get<Bonus>(); bonus->bonusType) {
+        case BonusType::PistolAmmo:
+            playerEntity.get_mut<Pistol>()->ammoCount += bonus->amount;
+            break;
+        case BonusType::ShotgunAmmo:
+            playerEntity.get_mut<Shotgun>()->ammoCount += bonus->amount;
+            break;
+        case BonusType::SmgAmmo:
+            playerEntity.get_mut<SubmachineGun>()->ammoCount += bonus->amount;
+            break;
+        case BonusType::Health:
+            playerEntity.get_mut<Health>()->amount += bonus->amount;
+            break;
+    }
+    bonusEntity.destruct();
+}
+
 void CollisionDetectionSystem(const flecs::world &ecsWorld) {
     const auto query = ecsWorld.query<const Position, const Collider>();
     const auto playerQuery = ecsWorld.query<const Player>();
@@ -43,25 +63,9 @@ void CollisionDetectionSystem(const flecs::world &ecsWorld) {
                                     playerScore->amount += 10;
                                     SpawnBonus(ecsWorld, enemyPosition);
                                 }
-                            } else if (collider1.IsPlayer() && collider2.IsBonus() || collider1.IsBonus() && collider2.
-                                       IsPlayer()) {
-                                const auto playerEntity = collider1.IsPlayer() ? entity1 : entity2;
-                                const auto bonusEntity = collider1.IsBonus() ? entity1 : entity2;
-                                switch (const auto bonus = bonusEntity.get<Bonus>(); bonus->bonusType) {
-                                    case BonusType::PistolAmmo:
-                                        playerEntity.get_mut<Pistol>()->ammoCount += bonus->amount;
-                                        break;
-                                    case BonusType::ShotgunAmmo:
-                                        playerEntity.get_mut<Shotgun>()->ammoCount += bonus->amount;
-                                        break;
-                                    case BonusType::SmgAmmo:
-                                        playerEntity.get_mut<SubmachineGun>()->ammoCount += bonus->amount;
-                                        break;
-                                    case BonusType::Health:
-                                        playerEntity.get_mut<Health>()->amount += bonus->amount;
-                                        break;
-                                }
-                                bonusEntity.destruct();
+                            } else if (collider1.IsPlayer() && collider2.IsBonus() ||
+                                       collider1.IsBonus() && collider2.IsPlayer()) {
+                                ApplyBonus(entity1, collider1, entity2);
                                 return;
                             }
 
